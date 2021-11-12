6d1d5bf58db683a4188e47dacb45ef2a6a2f9bcb
Shivansh Goel <shivanshgoel21@gmail.com>
2021-11-12 22:59:49 +0530

    add comments

--- app.py
+++ app.py
@@ -7,11 +7,13 @@ import pickle
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+# reading all the datasets from csv files
 df = pd.read_csv('./datasets/dataset.csv')
 df1 = pd.read_csv('./datasets/Symptom-severity.csv')
 description = pd.read_csv('./datasets/symptom_Description.csv')
 precaution = pd.read_csv('./datasets/symptom_precaution.csv')
 
+# pre-processing datas
 df.isna().sum()
 df.isnull().sum()
 
@@ -49,57 +51,56 @@ df['Disease'].unique()
 data = df.iloc[:,1:].values
 labels = df['Disease'].values
 
-# x_train, x_test, y_train, y_test = train_test_split(data, labels, shuffle=True, train_size = 0.80)
-# print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
 
-# model = SVC()
-# model.fit(x_train, y_train)
-
-# pickle.dump(model, open('model.sav', 'wb'))
-
+# loading the trained (pickled) model
 model = pickle.load(open('./model/model.sav', 'rb'))
 
-# preds = model.predict(x_test)
-# print('Preds', preds)
-
+# running flask app
 app = Flask(__name__)
 CORS(app)
 
-# print('Precaution ', precaution)
 
-
-def SVM(psymptoms, loc):
+# function to predict disease based on symptoms
+def SVM(symptoms, loc):
     
-    a = np.array(df1["Symptom"])
-    b = np.array(df1["weight"])
+    sym = np.array(df1["Symptom"])
+    wei = np.array(df1["weight"])
 
-    for j in range(len(psymptoms)):
-        for k in range(len(a)):
-            if psymptoms[j]==a[k]:
-                psymptoms[j]=b[k]
+    for j in range(len(symptoms)):
+        for k in range(len(sym)):
+            if symptoms[j]==sym[k]:
+                symptoms[j]=wei[k]
 
-    nulls = [0,0,0,0,0,0,0,0,0,0,0,0]
-    psy = [psymptoms + nulls]
+    total_length = 17
+    zeros_required = total_length - len(symptoms)
+    
+    nulls_required = [0] * zeros_required
 
-    pred2 = model.predict(psy)
+    total_symptoms = [symptoms + nulls_required]
+
+    pred2 = model.predict(total_symptoms)
 
     return list(pred2)
 
 
 @app.route('/', methods = ['POST'])
 def index():
+
+    # read the request body for symptoms and location
     symptoms = request.json['symptoms']
     location = request.json['location']
 
-
+    # predict the disease
     res = SVM(symptoms, location)
     res = res[0]
 
-    des = description[description.Disease == res]['Description']
-    prec = precaution[precaution.Disease == res]['Precaution_1']
+    # get the description and precautions for the predicted disease
+    des = description[description.Disease == res]['Description'].item()
+    prec = precaution[precaution.Disease == res]['Precaution_1'].item()
     des = str(des)
     prec = str(prec)
     
+    # return the result
     return jsonify(
         disease=res,
         description=des,
@@ -108,6 +109,8 @@ def index():
 
 @app.route('/disease')
 def disease():
+
+    # all the possible symptoms
     dis =  ["fatigue", "yellowish_skin", "loss_of_appetite", "yellowing_of_eyes", 'family_history',"stomach_pain", "ulcers_on_tongue", "vomiting", "cough", "chest_pain"]
     return jsonify(
         response=dis
@@ -115,6 +118,8 @@ def disease():
 
 @app.route('/location')
 def location():
+
+    # all the possible locations
     loc =  ["New Delhi", "Mumbai", "Chennai", "Kolkata", "Bengaluru"]
     return jsonify(
         response=loc
